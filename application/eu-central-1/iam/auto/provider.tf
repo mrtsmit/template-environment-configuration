f86f4480f5246884257c889d56f6a6803002c9fa
Radosław Palczyński <radoslaw.palczynski@nordcloud.com>
2018-12-05 13:46:16 +0100

    Add option to auto create IAM policy on application account

---
+++ application/eu-central-1/iam/auto/provider.tf
@@ -0,0 +1,9 @@
+# Parameters to set externally:
+# * cross-account role ARN (via variable)
+
+provider "aws" {
+  assume_role {
+    role_arn = "${var.iam_cross_account_role_arn}"
+  }
+  region = "${var.region}"
+}
