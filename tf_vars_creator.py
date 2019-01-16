6d94b89cb658d8889a72170e67b97f81f71a7170
Kartik Pisharodi <Kartik.Pisharodi@partner.bmwgroup.com>
2019-01-16 15:45:24 -0500

    made modifications so that the region is agnostic with the template, and any region can be used

--- tf_vars_creator.py
+++ tf_vars_creator.py
@@ -1,4 +1,5 @@
 from jinja2 import Template #this may require a pip install jinja2
+from distutils.dir_util import copy_tree
 
 #replaces values into the templated file, to add a value add curly braces: ex - {{value}}
 aws_account = raw_input("Enter your AWS Account Number (without hypens): ")
@@ -6,6 +7,7 @@ aws_opp_account = raw_input("Enter your AWS Operations Account Number (without h
 environment_type = raw_input("Enter your Environment Type (e.g. test, production): ")
 role_name = raw_input("Enter your Role Name (ex - arn:aws:iam::<account inherited from above>:role/<<role_name>>): ")
 region = raw_input("Enter your AWS Region: ")
+product_domain_name = raw_input("Enter the Product Domain Name: ")
 vpc_id = raw_input("Enter your VPC ID (must be pre-created): ")
 
 ## potentially calculate subnets using a vpc_id
@@ -33,11 +35,20 @@ gitBitbucketServer = raw_input("Enter your git Bitbucket Server: ")
 jxDomainHostedZoneID = raw_input("JX Domain Hosted Zone: ")
 jxDomainAliasPrefix = raw_input("JX Domain Alias Prefix: ")
 
-TEMPLATE_FILE = "operations/" + region + "/terraform.tfvars.template"
+# copy new folder structure
+fromDirectory = "operations/region"
+toDirectory = "operations/" + region + "/"
+copy_tree(fromDirectory, toDirectory)
+
+fromDirectory = "application/region"
+toDirectory = "application/" + region + "/"
+copy_tree(fromDirectory, toDirectory)
+
+TEMPLATE_FILE = "operations/region/terraform.tfvars.template"
 with open(TEMPLATE_FILE) as file_:
     template = Template(file_.read())
 #add value from top section here if newly added    
-rendered_file = template.render(aws_account=aws_account,environment_type=environment_type,role_name=role_name,aws_opp_account=aws_opp_account,region=region,vpc_id=vpc_id,subnet1_id=subnet1_id,subnet2_id=subnet2_id,subnet3_id=subnet3_id,jenkins_subnet_id=jenkins_subnet_id)
+rendered_file = template.render(aws_account=aws_account,environment_type=environment_type,role_name=role_name,aws_opp_account=aws_opp_account,region=region,vpc_id=vpc_id,subnet1_id=subnet1_id,subnet2_id=subnet2_id,subnet3_id=subnet3_id,jenkins_subnet_id=jenkins_subnet_id,product_domain_name=product_domain_name)
 f = open("operations/" + region + "/terraform.tfvars" , "w")
 f.write(rendered_file)
 print(rendered_file)
@@ -47,12 +58,12 @@ TEMPLATE_FILE = "application/" + region + "/terraform.tfvars.template"
 with open(TEMPLATE_FILE) as file_:
     template = Template(file_.read())
 #add value from top section here if newly added    
-rendered_file = template.render(aws_account=aws_account,environment_type=environment_type,role_name=role_name,aws_opp_account=aws_opp_account,region=region,vpc_id=vpc_id,subnet1_id=subnet1_id,subnet2_id=subnet2_id,subnet3_id=subnet3_id)
+rendered_file = template.render(aws_account=aws_account,environment_type=environment_type,role_name=role_name,aws_opp_account=aws_opp_account,region=region,vpc_id=vpc_id,subnet1_id=subnet1_id,subnet2_id=subnet2_id,subnet3_id=subnet3_id,product_domain_name=product_domain_name)
 f = open("application/" + region + "/terraform.tfvars" , "w")
 f.write(rendered_file)
 print(rendered_file)
 
-TEMPLATE_FILE = "operations/" + region + "/env/jenkins/parameters.yaml.template"
+TEMPLATE_FILE = "operations/region/env/jenkins/parameters.yaml.template"
 with open(TEMPLATE_FILE) as file_:
     template = Template(file_.read())
 #add value from top section here if newly added    
